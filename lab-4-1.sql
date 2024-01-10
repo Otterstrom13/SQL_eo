93585cbccdeb1ae6e2691cc36fe935e29a48e99a
Ethan Otterstrom <ethan.otterstrom@kellogg.northwestern.edu>
2024-01-10 20:41:27 +0000

    Lab 4.1 completed

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -3,4 +3,7 @@
 -- Expected result:
 -- 2935
 
-
+SELECT sum(stats.hits), players.first_name
+FROM stats INNER JOIN players on players.id = stats.player_id
+WHERE players.first_name = "Barry"
+AND players.last_name = "Bonds";
